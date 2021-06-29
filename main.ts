c528078b298bc763542fb855de8dc67bababa5fb
Manuel Ávila Rodríguez <mastercraft9000@gmail.com>
2021-06-28 22:15:01 -0500

    Env music and visual effects

--- main.ts
+++ main.ts
@@ -1,5 +1,15 @@
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+	
+})
 scene.setBackgroundImage(assets.image`bg`)
 effects.starField.startScreenEffect()
-let mySprite = sprites.create(assets.image`player`, SpriteKind.Player)
-mySprite.setStayInScreen(true)
-controller.moveSprite(mySprite, 100, 100)
+let thePlayer = sprites.create(assets.image`player_sprite_0`, SpriteKind.Player)
+let theEnemy = sprites.create(assets.image`sprite_enemy_0`, SpriteKind.Player)
+thePlayer.setStayInScreen(true)
+controller.moveSprite(thePlayer, 100, 100)
+while (true) {
+    music.playMelody("E B C5 A B G A F ", 90)
+}
