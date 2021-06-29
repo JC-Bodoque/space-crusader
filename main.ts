cd2d510e105e769a7d6d107a3b97a4276ede1858
Manuel Ávila Rodríguez <mastercraft9000@gmail.com>
2021-06-29 10:08:56 -0500

    More features

--- main.ts
+++ main.ts
@@ -4,6 +4,11 @@ controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     projectile = sprites.createProjectileFromSprite(assets.image`shoot`, thePlayer, 0, -50)
 })
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
+    sprite.destroy()
+    otherSprite.destroy(effects.disintegrate, 500)
+    info.changeScoreBy(1)
+})
 function setEnviromentMusic () {
     while (true) {
         music.playMelody("E B C5 A B G A F ", 90)
@@ -11,31 +16,27 @@ function setEnviromentMusic () {
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
-    thePlayer.destroy()
+    otherSprite.destroy(effects.disintegrate, 500)
+    scene.cameraShake(0, 0)
+    if (info.life() == 0) {
+        thePlayer.destroy(effects.disintegrate, 500)
+    }
 })
-let asteroid: Sprite = null
 let theEnemy: Sprite = null
 let projectile: Sprite = null
 let thePlayer: Sprite = null
-scene.setBackgroundImage(assets.image`bg`)
+let asteroid = null
+// scene.setBackgroundImage(assets.image`bg`)
 effects.starField.startScreenEffect()
 thePlayer = sprites.create(assets.image`player_sprite_0`, SpriteKind.Player)
 controller.moveSprite(thePlayer)
 thePlayer.setStayInScreen(true)
 info.setLife(5)
 info.setScore(0)
-setEnviromentMusic()
-game.onUpdate(function () {
-    theEnemy = sprites.create(assets.image`sprite_enemy_0`, SpriteKind.Enemy)
-    asteroid = sprites.create(assets.image`asteroid_sprite_0`, SpriteKind.Player)
-    asteroid.x = randint(10, scene.screenWidth() - 10)
-    asteroid.y = scene.screenHeight()
-    asteroid.setVelocity(0, 50)
-})
+// setEnviromentMusic()
 game.onUpdateInterval(500, function () {
     theEnemy = sprites.create(assets.image`sprite_enemy_0`, SpriteKind.Enemy)
-    asteroid = sprites.create(assets.image`asteroid_sprite_0`, SpriteKind.Player)
-    asteroid.x = randint(10, scene.screenWidth() - 10)
-    asteroid.y = scene.screenHeight()
-    asteroid.setVelocity(0, 50)
+    theEnemy.x = randint(10, scene.screenWidth() - 10)
+    theEnemy.y = 0
+    theEnemy.setVelocity(0, 50)
 })
