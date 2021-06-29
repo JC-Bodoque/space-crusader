506de87a1c474d01e038d0317e790af4219b9df0
Manuel Ávila Rodríguez <mastercraft9000@gmail.com>
2021-06-29 10:33:20 -0500

    Enemies life bar

--- main.ts
+++ main.ts
@@ -6,8 +6,11 @@ controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Enemy, function (sprite, otherSprite) {
     sprite.destroy()
-    otherSprite.destroy(effects.disintegrate, 500)
     info.changeScoreBy(1)
+    statusbars.getStatusBarAttachedTo(StatusBarKind.EnemyHealth, otherSprite).value += -5
+    if (statusbar.value == 0) {
+        otherSprite.destroy(effects.disintegrate, 500)
+    }
 })
 function setEnviromentMusic () {
     while (true) {
@@ -23,6 +26,7 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSp
     }
 })
 let theEnemy: Sprite = null
+let statusbar: StatusBarSprite = null
 let projectile: Sprite = null
 let thePlayer: Sprite = null
 let asteroid = null
@@ -34,9 +38,12 @@ thePlayer.setStayInScreen(true)
 info.setLife(5)
 info.setScore(0)
 // setEnviromentMusic()
+// statusbar.setBarSize(20, 4)
 game.onUpdateInterval(500, function () {
     theEnemy = sprites.create(assets.image`sprite_enemy_0`, SpriteKind.Enemy)
     theEnemy.x = randint(10, scene.screenWidth() - 10)
     theEnemy.y = 0
     theEnemy.setVelocity(0, 50)
+    statusbar = statusbars.create(20, 4, StatusBarKind.EnemyHealth)
+    statusbar.attachToSprite(theEnemy)
 })
